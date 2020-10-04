3f21de402f44aca7222d1327a337c97684277bcd
Username <nokyung.nabhisa@gmail.com>
2020-10-04 15:39:50 +0700

    ready to push

---
+++ stock/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'frontend/index.html')
+
+def about(request):
+    return render(request, 'frontend/about.html')
+
+def contact(request):
+    return render(request, 'frontend/contact.html')
